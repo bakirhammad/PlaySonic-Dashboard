6640d8cf067d95437abbc5496394f57dae78ae87
haithamassoli <haitham.b.assoli@gmail.com>
2025-02-17 09:44:16 +0300

    fix: update GetReservationList URL to include trailing ampersand

--- src/domain/enums/URL/Reservation/reservationUrls/Reservation.ts
+++ src/domain/enums/URL/Reservation/reservationUrls/Reservation.ts
@@ -1,5 +1,5 @@
 export enum ReservationUrlEnum {
-  GetReservationList = "Booking/Reservation/List?orderBy=id&OrderDirection=false",
+  GetReservationList = "Booking/Reservation/List?orderBy=id&OrderDirection=false&",
   GetReservationById = "Booking/Reservation/GetById?",
   CreateReservation = "Booking/Reservation/Create",
   UpdateReservation = "Booking/Reservation/Update",
